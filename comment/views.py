8b6beef253e191fc6e25b98b9e755e9f5ad9c020
mandrej <milan.andrejevic@gmail.com>
2013-01-06 22:10:53 +0100

    comments in progress with bugs

--- comment/views.py
+++ comment/views.py
@@ -1,14 +1,13 @@
-import webapp2
+import webapp2, json
 from google.appengine.api import users
 from google.appengine.ext import ndb, deferred
-#from django.shortcuts import redirect, render
-#from django.core.exceptions import PermissionDenied
-#from django.utils.translation import gettext_lazy as _
-#from django import forms
+from webapp2_extras.i18n import lazy_gettext as _
+from wtforms import Form, widgets, fields, validators
 from models import Comment
-#from lib.comm import Paginator, Filter, login_required, json_response
-from common import  LIMIT, TIMEOUT, BaseHandler, Paginator, Filter
+from common import  BaseHandler, Paginator, Filter, EmailField
+from settings import LIMIT, TIMEOUT
 PER_PAGE = 10
+import logging
 
 class Index(BaseHandler):
     def get(self, field=None, value=None):
@@ -30,55 +29,35 @@ class Index(BaseHandler):
                 'has_previous': page > 1}
         self.render_template('comment/index.html', data)
 
-#class CommentForm(forms.Form):
-#    email = forms.EmailField(label='e-mail', required=True,
-#                    widget=forms.TextInput(),
-#                    error_messages={'required': _('Required field'),
-#                                    'invalid': _('Enter a valid e-mail address')})
-#    body = forms.CharField(label=_('Comment'),
-#                    widget=forms.Textarea(attrs={'rows': 6, 'cols': 16}),
-#                    error_messages={'required': _('Required field')})
-#
-#    def clean_email(self):
-#        data = self.cleaned_data['email']
-#        if not data:
-#            user = users.get_current_user()
-#            if user:
-#                data = user.email()
-#            else:
-#                raise forms.ValidationError(_('Required field'))
-#        return data
-#
-#def send(safekey, email, body):
-#    key = ndb.Key(urlsafe=safekey)
-#    obj = Comment(parent=key,
-#                  author = users.User(email),
-#                  forkind = key.kind(),
-#                  body=body)
-#    obj.add()
-#
-#def validate(request):
-#    if request.is_ajax():
-#        params = request.POST
-#        form = CommentForm(params)
-#        if form.is_valid():
-#            data = form.cleaned_data
-#            #{u'data': [{u'body': u''}], u'key': u'agRh...'}
-#            args = [params['safekey'], data['email'], data['body']]
-#            deferred.defer(send, *args)
-#            return json_response({'success': True})
-#
-#        ret = {}
-#        for k, v in form.errors.items():
-#            ret[k] = v.as_text()
-#        return json_response(ret)
-#
-#def add(request, safekey, tmpl='snippets/addcomment.html'):
-#    if request.is_ajax():
-#        form = CommentForm()
-#        refobj = ndb.Key(urlsafe=safekey).get()
-#        user = users.get_current_user()
-#        if user:
-#            form.fields['email'].initial = user.email()
-#        data = {'refobj': refobj, 'form': form}
-#        return render(request, tmpl, data)
+class AddForm(Form):
+    email = fields.TextField(_('e-mail'), validators=[validators.DataRequired(), validators.Email()])
+    body = fields.TextAreaField(_('Comment'), validators=[validators.DataRequired()])
+
+def send(safekey, email, body):
+    key = ndb.Key(urlsafe=safekey)
+    obj = Comment(parent=key,
+                  author = users.User(email),
+                  forkind = key.kind(),
+                  body=body)
+    obj.add()
+
+class Add(BaseHandler):
+    def get(self, safekey, form=None):
+        refobj = ndb.Key(urlsafe=safekey).get()
+        user = users.get_current_user()
+        if form is None:
+            if user:
+                form = AddForm(**{'email': user.email()})
+            else:
+                form = AddForm()
+        self.render_template('snippets/addcomment.html', {'form': form, 'safekey': safekey, 'headline': refobj.headline})
+
+    def post(self, safekey):
+        form = AddForm(formdata=self.request.POST)
+        self.response = webapp2.Response(content_type='application/json')
+        self.response.headers['X-Requested-With'] = 'XMLHttpRequest'
+        if form.validate():
+            deferred.defer(send, safekey, form.data['email'], form.data['body'])
+            self.response.out.write(json.dumps({'success': True}))
+        else:
+            self.response.out.write(json.dumps(form.errors))
