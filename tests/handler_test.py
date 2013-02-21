c68c434acccc6368e3835683aaba38d3b4993081
mandrej <milan.andrejevic@gmail.com>
2013-02-21 09:52:39 +0100

    handler tests

---
+++ tests/handler_test.py
@@ -0,0 +1,13 @@
+__author__ = 'milan'
+
+import unittest
+import webapp2
+import main
+
+
+class HandlerTest(unittest.TestCase):
+    def test_Sign(self):
+        req = webapp2.Request.blank('/sign')
+        res = req.get_response(main.app)
+        location = res.headers.get('Location')
+        print location
