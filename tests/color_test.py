c7bee69efaaba669caddb186bdca26e69ce4c7b4
mandrej <milan.andrejevic@gmail.com>
2013-03-07 22:50:21 +0100

    color mean test

--- tests/color_test.py
+++ tests/color_test.py
@@ -1,7 +1,8 @@
 __author__ = 'milan'
 
 import unittest
-from colormath.color_objects import HSLColor
+import numpy as np
+from colormath.color_objects import HSLColor, RGBColor
 from models import Photo
 from collections import defaultdict
 from settings import HUE
@@ -14,10 +15,20 @@ class ColorTest(unittest.TestCase):
             h, l, s = obj.hls
             color = HSLColor(h, s / 100.0, l / 100.0)
             values = color.get_value_tuple() + color.convert_to('lab').get_value_tuple()
-            print('{0},{1},{2},{3},{4:.2f},{5:.2f},{6:.2f}'.format(obj.key.string_id(), *values))
+            print '{0},{1},{2},{3},{4:.2f},{5:.2f},{6:.2f}'.format(obj.key.string_id(), *values)
+
+    def test_mean(self):
+        colors = []
+        query = Photo.query()
+        for obj in query:
+            color = RGBColor(*obj.rgb)
+            colors.append(color.convert_to('hsl').get_value_tuple())
+
+        print 'HUE mean {0:.2f}\nSAT mean {1:.2f}\nLUM mean {2:.2f}'.format(*tuple(np.mean(colors, axis=0)))
+        print
+        print 'HUE std {0:.2f}\nSAT std {1:.2f}\nLUM std {2:.2f}'.format(*tuple(np.std(colors, axis=0)))
 
     def test_spectra(self):
-
         spectra = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
         for sat in (20, 60,):
             for lum in (40, 80,):
