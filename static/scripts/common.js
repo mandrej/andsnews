01e38058b0f23bfecdd3e94776ecf8244def73ba
mandrej <milan.andrejevic@gmail.com>
2013-12-15 11:14:03 +0100

    add comment callback, avoiding snippets.addcomment.js

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -169,6 +169,36 @@ var autoCompleteOptions = {
         evt.preventDefault();
         $('#addcomment').load(this.href, function() {
             $('#overlay, #addcomment').show();
+            $('#body').markItUp(cmntSettings, {nameSpace: 'small'});
+            $('.content button').click(function(evt) {
+                evt.preventDefault();
+                $.ajax({
+                    type: 'POST',
+                    url: $('#addcommentForm').attr('action'),
+                    data: {'body': $('#body').val(), 'token': token},
+                    success: function(data) {
+                        if (typeof(data) == 'string') {
+                            $('.dummy').hide();
+                            $('.info').show()
+                            $('.comments').prepend(data);
+                            $('a.confirm').click(function(e) {
+                                e.preventDefault();
+                                $('#confirm').load(this.href, function() {
+                                    $('#overlay, #confirm').show();
+                                });
+                            });
+                            setTimeout(function() {
+                                $('#overlay, #addcomment').hide();
+                            }, 1000);
+                        } else {
+                            $('.error').text('');
+                            $.each(data, function(key, arr) {
+                                $('.error.C_' + key).text(arr.join(', '));
+                            });
+                        }
+                    }
+                });
+            });
         });
     });
     // tools
