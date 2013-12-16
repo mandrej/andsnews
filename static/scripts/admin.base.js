c3a1b81c6def7ee769eb40750bf91ca0ecc476b9
mandrej <milan.andrejevic@gmail.com>
2013-12-16 21:38:38 +0100

    avoiding snippets.graph.js

--- static/scripts/admin.base.js
+++ static/scripts/admin.base.js
@@ -1,18 +1,8 @@
 /**
  * Created by milan on 12/14/13.
  */
-$(document).ready(function () {
-    $('#main button, #sidebar button').click(function() {
-        $('#overlay, #spinner').show();
-    });
-    $('.ff, .ss').collapseGroup();
-    $('.zebra').colStyle();
-    $('.menu').click(function(evt) {
-        var disallow = {"A": 1, "BUTTON": 1, "INPUT": 1};
-        var parnetTagName = $(evt.target).parent()[0].tagName;
-        if (!(disallow[evt.target.tagName] || disallow[parnetTagName])) {
-            $('.menu').toggle('slow');
-            evt.preventDefault();
-        }
-    });
+$('#main button, #sidebar button').click(function() {
+    $('#overlay, #spinner').show();
 });
+$('.ff, .ss').collapseGroup();
+$('.zebra').colStyle();
--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,128 +1,126 @@
-$(document).ready(function () {
-    var $pinner = $('#overlay, #spinner');
-    // autocomplete
-    var autoCompleteOptions = {
-        width: 284,
-        selectFirst: false,
-        multiple: false,
-        matchContains: true
-    };
+var $pinner = $('#overlay, #spinner');
+// autocomplete
+var autoCompleteOptions = {
+    width: 284,
+    selectFirst: false,
+    multiple: false,
+    matchContains: true
+};
 
-    $('.collapse').click(function(evt) {
-        evt.preventDefault();
-        var cntx = $(this);
-        var url = cntx.attr('href');
-        var key = url.split('/').pop();
-        var field = key.split('_').pop();
+$('.collapse').click(function(evt) {
+    evt.preventDefault();
+    var cntx = $(this);
+    var url = cntx.attr('href');
+    var key = url.split('/').pop();
+    var field = key.split('_').pop();
 
-        $('.hide').slideUp();
-        $('.collapse').each(function() {
-            $(this).plus();
-        });
-
-        $pinner.show();
-        if (crumbs[0] == field && crumbs[1] != undefined) url += '/' + crumbs[1];
-        $.ajax({
-            url: url,
-            context: $('#' + field + 'cloud'),
-            success: function(snippet) {
-                $(this).html(snippet).slideDown();
-                cntx.minus();
-                $pinner.hide();
-            }
-        });
+    $('.hide').slideUp();
+    $('.collapse').each(function() {
+        $(this).plus();
     });
 
-    $('#searchForm').submit(function() {
-        $pinner.show();
+    $pinner.show();
+    if (crumbs[0] == field && crumbs[1] != undefined) url += '/' + crumbs[1];
+    $.ajax({
+        url: url,
+        context: $('#' + field + 'cloud'),
+        success: function(snippet) {
+            $(this).html(snippet).slideDown();
+            cntx.minus();
+            $pinner.hide();
+        }
+    });
+});
+
+$('#searchForm').submit(function() {
+    $pinner.show();
+});
+
+// search highlight
+var matchFind = location.search.match(/find=([^&]+)/i);
+if (matchFind) {
+    $(document).SearchHighlight({exact: "partial", highlight: "#main", keys: decodeURIComponent(matchFind[1])});
+}
+// startup
+var crumbs = location.pathname.split('/');
+crumbs.shift();
+crumbs.shift();
+
+if (kind != '') {
+    $('.hide').hide();
+    $('.collapse').each(function() {
+        $(this).plus();
     });
 
-    // search highlight
-    var matchFind = location.search.match(/find=([^&]+)/i);
-    if (matchFind) {
-        $(document).SearchHighlight({exact: "partial", highlight: "#main", keys: decodeURIComponent(matchFind[1])});
-    }
-    // startup
-    var crumbs = location.pathname.split('/');
-    crumbs.shift();
-    crumbs.shift();
-
-    if (kind != '') {
-        $('.hide').hide();
-        $('.collapse').each(function() {
-            $(this).plus();
-        });
-
-        if (crumbs.length >= 2) {
-            var field = crumbs[0];
-            var available = ['tags', 'date', 'author', 'color', 'model', 'lens', 'eqv', 'iso', 'forkind'];
-            if ($.inArray(field, available) != -1) {
-                $.ajax({
-                    url: '/filter/' + kind + '_' + field + '/' + crumbs[1],
-                    context: $('#' + field + 'cloud'),
-                    success: function(snippet) {
-                        $(this).html(snippet).slideDown();
-                        $('.' + field).minus();
-                    }
-                });
-            }
+    if (crumbs.length >= 2) {
+        var field = crumbs[0];
+        var available = ['tags', 'date', 'author', 'color', 'model', 'lens', 'eqv', 'iso', 'forkind'];
+        if ($.inArray(field, available) != -1) {
+            $.ajax({
+                url: '/filter/' + kind + '_' + field + '/' + crumbs[1],
+                context: $('#' + field + 'cloud'),
+                success: function(snippet) {
+                    $(this).html(snippet).slideDown();
+                    $('.' + field).minus();
+                }
+            });
         }
     }
+}
 
-    // confirm
-    $('a.confirm').click(function(evt) {
-        evt.preventDefault();
-        $('#confirm').load(this.href, function() {
-            $('#overlay, #confirm').show();
-            $('.content button').click(function() {
-                $('#overlay, #spinner').show();
-            });
+// confirm
+$('a.confirm').click(function(evt) {
+    evt.preventDefault();
+    $('#confirm').load(this.href, function() {
+        $('#overlay, #confirm').show();
+        $('.content button').click(function() {
+            $('#overlay, #spinner').show();
         });
     });
-    // add comment
-    $('a.comment_add').click(function(evt) {
-        evt.preventDefault();
-        $('#addcomment').load(this.href, function() {
-            $('#overlay, #addcomment').show();
-            $('#body').markItUp(cmntSettings, {nameSpace: 'small'});
-            $('.content button').click(function(evt) {
-                evt.preventDefault();
-                $.ajax({
-                    type: 'POST',
-                    url: $('#addcommentForm').attr('action'),
-                    data: {'body': $('#body').val(), 'token': token},
-                    success: function(data) {
-                        if (typeof(data) == 'string') {
-                            $('.dummy').hide();
-                            $('.info').show()
-                            $('.comments').prepend(data);
-                            $('a.confirm').click(function(e) {
-                                e.preventDefault();
-                                $('#confirm').load(this.href, function() {
-                                    $('#overlay, #confirm').show();
-                                });
+});
+// add comment
+$('a.comment_add').click(function(evt) {
+    evt.preventDefault();
+    $('#addcomment').load(this.href, function() {
+        $('#overlay, #addcomment').show();
+        $('#body').markItUp(cmntSettings, {nameSpace: 'small'});
+        $('.content button').click(function(evt) {
+            evt.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: $('#addcommentForm').attr('action'),
+                data: {'body': $('#body').val(), 'token': token},
+                success: function(data) {
+                    if (typeof(data) == 'string') {
+                        $('.dummy').hide();
+                        $('.info').show()
+                        $('.comments').prepend(data);
+                        $('a.confirm').click(function(e) {
+                            e.preventDefault();
+                            $('#confirm').load(this.href, function() {
+                                $('#overlay, #confirm').show();
                             });
-                            setTimeout(function() {
-                                $('#overlay, #addcomment').hide();
-                            }, 1000);
-                        } else {
-                            $('.error').text('');
-                            $.each(data, function(key, arr) {
-                                $('.error.C_' + key).text(arr.join(', '));
-                            });
-                        }
+                        });
+                        setTimeout(function() {
+                            $('#overlay, #addcomment').hide();
+                        }, 1000);
+                    } else {
+                        $('.error').text('');
+                        $.each(data, function(key, arr) {
+                            $('.error.C_' + key).text(arr.join(', '));
+                        });
                     }
-                });
+                }
             });
         });
     });
+});
 
-    // masonry
-    var $container = $('#container');
-    $container.imagesLoaded(function() {
-        $container.masonry({
-            itemSelector: '.brick'
-        });
-        $container.masonry('bindResize');
+// masonry
+var $container = $('#container');
+$container.imagesLoaded(function() {
+    $container.masonry({
+        itemSelector: '.brick'
     });
+    $container.masonry('bindResize');
 });
