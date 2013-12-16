84452e06d03ba72d6996f51b58839a4905ade843
mandrej <milan.andrejevic@gmail.com>
2013-12-16 19:46:15 +0100

    $(document).ready on last script

--- static/scripts/admin.base.js
+++ static/scripts/admin.base.js
@@ -1,16 +1,18 @@
 /**
  * Created by milan on 12/14/13.
  */
-$('#main button, #sidebar button').click(function() {
-    $('#overlay, #spinner').show();
-});
-$('.ff, .ss').collapseGroup();
-$('.zebra').colStyle();
-$('.menu').click(function(evt) {
-    var disallow = {"A": 1, "BUTTON": 1, "INPUT": 1};
-    var parnetTagName = $(evt.target).parent()[0].tagName;
-    if (!(disallow[evt.target.tagName] || disallow[parnetTagName])) {
-        $('.menu').toggle('slow');
-        evt.preventDefault();
-    }
+$(document).ready(function () {
+    $('#main button, #sidebar button').click(function() {
+        $('#overlay, #spinner').show();
+    });
+    $('.ff, .ss').collapseGroup();
+    $('.zebra').colStyle();
+    $('.menu').click(function(evt) {
+        var disallow = {"A": 1, "BUTTON": 1, "INPUT": 1};
+        var parnetTagName = $(evt.target).parent()[0].tagName;
+        if (!(disallow[evt.target.tagName] || disallow[parnetTagName])) {
+            $('.menu').toggle('slow');
+            evt.preventDefault();
+        }
+    });
 });
