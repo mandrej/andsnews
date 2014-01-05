ce1347a15421fe565cc2dd4041143b599b965a41
mandrej <milan.andrejevic@gmail.com>
2014-01-05 12:41:37 +0100

    exclude PT Sans

--- config.py
+++ config.py
@@ -23,8 +23,6 @@ LANGUAGES = (
 OFFLINE = """CACHE MANIFEST
 # %s
 https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js
-https://themes.googleusercontent.com/static/fonts/ptsans/v5/yrzXiAvgeQQdopyG8QSg8Q.woff
-https://themes.googleusercontent.com/static/fonts/ptsans/v5/g46X4VH_KHOWAAa-HpnGPhsxEYwM7FgeyaSgU71cLG0.woff
 /static/font/fontawesome.woff
 /static/images/bg.jpg
 /static/images/front.png
