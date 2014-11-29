2c849ac3ca50c9f4873ac82db13f29e1c46283a9
mandrej <milan.andrejevic@gmail.com>
2014-11-29 22:57:41 +0100

    current_fix

--- views/mapreduce.py
+++ views/mapreduce.py
@@ -5,8 +5,10 @@ from models import update_doc, rounding, img_palette, range_names
 from config import ASA, LENGTHS
 import logging
 
+
 def indexer(entity):
     update_doc(**entity.index_data)
+
 
 def calculate_palette(entity):
     logging.info(entity.headline)
@@ -20,11 +22,16 @@ def calculate_palette(entity):
         entity.rgb = palette.colors[0].value
     entity.hue, entity.lum, entity.sat = range_names(entity.rgb)
 
-    # if entity.focal_length and entity.crop_factor:
-    #     value = int(entity.focal_length * entity.crop_factor)
-    #     entity.eqv = rounding(value, LENGTHS)
-    # if entity.iso:
-    #     value = int(entity.iso)
-    #     entity.iso = rounding(value, ASA)
+    entity.put()
+
+
+def current_fix(entity):
+    logging.info(entity.headline)
+    if entity.focal_length and entity.crop_factor:
+        value = int(entity.focal_length * entity.crop_factor)
+        entity.eqv = rounding(value, LENGTHS)
+    if entity.iso:
+        value = int(entity.iso)
+        entity.iso = rounding(value, ASA)
 
     entity.put()
