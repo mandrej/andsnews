33400f4c2707aa672ccbcc19bcff7470d616a274
milan_andrejevic <milan.andrejevic@gmail.com>
2016-07-19 15:44:23 +0200

    moved app/ to client/ directory

--- sw-precache-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  staticFileGlobs: [
-    '/app/index.html',
-    '/manifest.json',
-    '/app/bower_components/webcomponentsjs/webcomponents-lite.min.js'
-  ],
-  navigateFallback: '/app/index.html'
-};
