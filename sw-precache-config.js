20789b78c8b4f009ebb3b931a3970dbd1243ad7f
milan_andrejevic <milan.andrejevic@gmail.com>
2016-07-19 14:22:59 +0200

    using polymer-cli

---
+++ sw-precache-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  staticFileGlobs: [
+    '/app/index.html',
+    '/manifest.json',
+    '/app/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/app/index.html'
+};
