d5b75bb55e344082e2f00bd71d120911383ef0e0
milan_andrejevic <milan.andrejevic@gmail.com>
2016-07-23 18:50:30 +0200

    search fix, dialog fix, precache config

--- client/sw-precache-config.js
+++ client/sw-precache-config.js
@@ -2,8 +2,19 @@ module.exports = {
     staticFileGlobs: [
         '/index.html',
         '/manifest.json',
-        '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+        '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+        '/images/*'
     ],
     navigateFallback: '/index.html',
-    navigateFallbackWhitelist: [/^(?!.*\.html$).*/]
+    navigateFallbackWhitelist: [/^(?!.*\.html$).*/],
+    runtimeCaching: [{
+        urlPattern: /\/api\/.*/,
+        handler: 'fastest',
+        options: {
+            cache: {
+                maxEntries: 10,
+                name: 'data-cache'
+            }
+        }
+    }]
 };
