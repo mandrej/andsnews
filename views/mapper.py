4f9010e8468bc85d9b3d6c7a39bf93ff85571f94
milan_andrejevic <milan.andrejevic@gmail.com>
2016-09-29 16:00:50 +0200

    blobstore to gcs migration

--- views/mapper.py
+++ views/mapper.py
@@ -2,11 +2,13 @@ import json
 import logging
 import itertools
 import collections
-from google.appengine.ext import ndb, deferred
+import cloudstorage as gcs
+from google.appengine.ext import ndb, deferred, blobstore
 from google.appengine.api import channel
 from google.appengine.api.datastore_errors import Timeout
 from google.appengine.runtime import DeadlineExceededError
 from models import Counter
+from config import BUCKET
 
 
 class Mapper(object):
@@ -142,3 +144,22 @@ class Builder(Mapper):
 
             channel.send_message(self.CHANNEL_NAME, json.dumps({'message': '%s %s' % (value, count)}))
         channel.send_message(self.CHANNEL_NAME, json.dumps({'message': 'END'}))
+
+
+# def current_fix(entity):
+#     blob_info = blobstore.BlobInfo.get(entity.blob_key)  # content_type, creation, filename, size
+#     if blob_info is not None:
+#         blob_reader = blobstore.BlobReader(entity.blob_key, buffer_size=1024*1024)
+#         buff = blob_reader.read(size=-1)
+#         object_name = BUCKET + blob_info.filename  # format /bucket/object
+#         write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+#         with gcs.open(
+#             object_name,
+#             'w',
+#             content_type=blob_info.content_type,
+#             retry_params=write_retry_params) as f:
+#             f.write(buff)  # <class 'cloudstorage.storage_api.StreamingBuffer'>
+#
+#         gcs_object_name = '/gs' + object_name
+#         entity.blob_key = blobstore.BlobKey(blobstore.create_gs_key(gcs_object_name))
+#         yield op.db.Put(entity)
