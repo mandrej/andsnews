3490bae16d492b8499f7ab5df8972243cb355790
milan_andrejevic <milan.andrejevic@gmail.com>
2018-11-27 22:53:01 +0100

    update

--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'Andрејевићи',
     themeColor: '#ffffff',
     msTileColor: '#ffffff',
-    assetsVersion: '201811272154',
+    assetsVersion: '201811272249',
     workboxOptions: {
       runtimeCaching: [
         {
