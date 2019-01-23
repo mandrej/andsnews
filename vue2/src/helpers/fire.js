6643037a0e4a749d427559bfd7c471612c4e626a
Milan Andrejevic <milan.andrejevic@gmail.com>
2019-01-23 21:00:24 +0100

    update

--- vue2/src/helpers/fire.js
+++ vue2/src/helpers/fire.js
@@ -9,10 +9,3 @@ const config = {
   messagingSenderId: '719127177629'
 }
 export const FB = firebase.initializeApp(config)
-
-// messaging.usePublicVapidKey('BEMvPS8oRWveXcM6M_uBdQvDFZqvYKUOnUa22hVvMMlSMFr_04rI3G3BjJWW7EZKSqkM2mchPP3tReV4LY0Y45o')
-// export const MESSAGING_AUTH = {
-//   'Content-Type': 'application/json',
-//   'Authorization': 'key=AAAAp29R6Z0:APA91bEsmfN6-ZywxP05Xpw-Ooto5FwTyXjgxRcqDllaTo6Kay3y8wnB-1QcwBt1-iQvoPt2p8wwp0JmKz4xWHST-pAQyrAivyTT-RFXasRYGmT09rP6oMuW95XHEi-HANZgzljuA-4i4Gh44den43zooddq5uSjBA',
-//   'project_id': config.messagingSenderId
-// }
