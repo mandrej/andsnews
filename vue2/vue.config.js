7b486d7b40dfa938db67d608b2df3d9eef35aed7
Milan Andrejevic <milan.andrejevic@gmail.com>
2019-03-09 22:20:20 +0100

    initial

--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -1,9 +1,23 @@
+const path = require('path')
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
 module.exports = {
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  },
+
+  configureWebpack: {
+    plugins: [
+      new VuetifyLoaderPlugin()
+    ]
+  },
+
   pwa: {
     name: 'Andрејевићи',
     themeColor: '#ffffff',
     msTileColor: '#ffffff',
-    assetsVersion: '201903091303',
+    assetsVersion: '201903092043',
     workboxOptions: {
       runtimeCaching: [
         {
@@ -58,4 +72,14 @@ module.exports = {
   parallel: undefined,
   css: undefined,
   lintOnSave: undefined
+}
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/imports.styl'),
+      ]
+    })
 }
