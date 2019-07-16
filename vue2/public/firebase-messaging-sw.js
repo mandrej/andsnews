df4aa2b943686d97f836a312bcc482f9adbc76fc
Milan Andrejevic <milan.andrejevic@gmail.com>
2019-07-16 21:56:48 +0200

    jshint eslint

--- vue2/public/firebase-messaging-sw.js
+++ vue2/public/firebase-messaging-sw.js
@@ -1,15 +1,21 @@
+/* jshint undef: false */
+/* jshint unused: false */
+
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.3.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/6.3.0/firebase-messaging.js')
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
   'messagingSenderId': '719127177629'
-});
+})
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
-const messaging = firebase.messaging();
+const messaging = firebase.messaging()
