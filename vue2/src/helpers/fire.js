025906e4eafc3c56d196e80e2aad6390d4460e11
Milan Andrejevic <mandrejevic@groundlink.com>
2019-10-21 12:32:30 +0200

    fix firebase bug add appId

--- vue2/src/helpers/fire.js
+++ vue2/src/helpers/fire.js
@@ -6,6 +6,7 @@ const config = {
   databaseURL: 'https://andsnews.firebaseio.com',
   projectId: 'andsnews',
   storageBucket: 'andsnews.appspot.com',
-  messagingSenderId: '719127177629'
+  messagingSenderId: '719127177629',
+  appId: '1:719127177629:web:5e1335058fa70551186774'
 }
 export default firebase.initializeApp(config)
