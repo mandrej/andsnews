d196b7174f1fdd466b55f8b24ba77dcd3868a3f5
mandrejevic <mandrejevic@groundlink.com>
2019-12-08 11:58:08 +0100

    gray icons

--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'Andрејевићи',
     themeColor: '#ffffff',
     msTileColor: '#ffffff',
-    assetsVersion: '201912061145',
+    assetsVersion: '201912081154',
     iconPaths: {
       favicon32: 'static/img/icons/favicon-32x32.png',
       favicon16: 'static/img/icons/favicon-16x16.png',
