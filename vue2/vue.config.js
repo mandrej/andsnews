8eaf5c3a2527525689dea032c221bacad84bedff
mandrejevic <mandrejevic@groundlink.com>
2020-01-17 13:28:57 +0100

    vue2.yaml use python2.7

--- vue2/vue.config.js
+++ vue2/vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'Andрејевићи',
     themeColor: '#ffffff',
     msTileColor: '#ffffff',
-    assetsVersion: '202001171250',
+    assetsVersion: '202001171315',
     iconPaths: {
       favicon32: 'static/img/icons/favicon-32x32.png',
       favicon16: 'static/img/icons/favicon-16x16.png',
