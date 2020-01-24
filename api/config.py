bddff269087cd16cfd487cfc438df33cd46273f8
mandrejevic <mandrejevic@groundlink.com>
2020-01-24 12:36:03 +0100

    ONE config.json

--- api/config.py
+++ api/config.py
@@ -1,46 +1,9 @@
-CONFIG = {
-    "firebase": {
-        "apiKey": "AIzaSyBj5uKo0P_mir_ChQ_syx_kUQ_g7nkNy6M",
-        "authDomain": "andsnews.firebaseapp.com",
-        "databaseURL": "https://andsnews.firebaseio.com",
-        "projectId": "andsnews",
-        "storageBucket": "andsnews.appspot.com",
-        "messagingSenderId": "719127177629",
-        "appId": "1: 719127177629:web: 5e1335058fa70551186774"
-    },
-    "fcm_server_key": "AAAAp29R6Z0:APA91bEsmfN6-ZywxP05Xpw-Ooto5FwTyXjgxRcqDllaTo6Kay3y8wnB-1QcwBt1-iQvoPt2p8wwp0JmKz4xWHST-pAQyrAivyTT-RFXasRYGmT09rP6oMuW95XHEi-HANZgzljuA-4i4Gh44den43zooddq5uSjBA",
-    "fcm_send": "https://fcm.googleapis.com/fcm/send",
-    "start_message": "*** START ***",
-    "end_message": "*** END ***",
-    "photo_filter": [
-        "year",
-        "tags",
-        "model",
-        "email"
-    ],
-    "cache_control": "public, max-age=86400",
-    "asa": [
-        50,
-        64,
-        80,
-        100,
-        125,
-        160,
-        200,
-        250,
-        320,
-        400,
-        500,
-        640,
-        800,
-        1000,
-        1250,
-        1600,
-        2000,
-        2500,
-        3200,
-        4000,
-        5000,
-        6400
-    ]
-}
+import os
+import json
+
+CONFIG = {}
+current_directory = os.path.dirname(__file__)
+parent_directory = os.path.split(current_directory)[0]
+
+with open(os.path.join(parent_directory, 'config.json')) as file:
+    CONFIG = json.load(file)
--- vue2/src/helpers/config.js
+++ vue2/src/helpers/config.js
@@ -1,14 +1,2 @@
-const CONFIG = {
-  'firebase': {
-    'apiKey': 'AIzaSyBj5uKo0P_mir_ChQ_syx_kUQ_g7nkNy6M',
-    'authDomain': 'andsnews.firebaseapp.com',
-    'databaseURL': 'https://andsnews.firebaseio.com',
-    'projectId': 'andsnews',
-    'storageBucket': 'andsnews.appspot.com',
-    'messagingSenderId': '719127177629',
-    'appId': '1: 719127177629:web: 5e1335058fa70551186774'
-  },
-  'google_storage_url': 'https://storage.googleapis.com/andsnews.appspot.com/',
-  'limit': 24
-}
+import CONFIG from '../../../config.json'
 export default CONFIG
