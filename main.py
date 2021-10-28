487fe8cb0843a24c332b5495b7fbfb1af4c54f57
Milan Andrejevic <milan.andrejevic@gmail.com>
2021-10-28 18:02:02 +0200

    Home, warmup service

--- main.py
+++ main.py
@@ -208,6 +208,11 @@ def push():
     return push_message(token, message)
 
 
+@app.route('/api/_ah/warmup')
+def warmup():
+    return 'warmup', 200, {}
+
+
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
--- vue2/src/views/Home.vue
+++ vue2/src/views/Home.vue
@@ -2,7 +2,6 @@
   <div
     v-lazy:background-image="getImgSrc(last)"
     class="d-flex flex-column fill-height align-center justify-center lazy last"
-    @click="$router.push({name: 'list', query: query})"
     style="cursor: pointer"
   >
     <div class="pa-5" style="position: absolute; top: 0; right: 0">
@@ -12,18 +11,36 @@
         {{count}} photos since 2007 and counting
       </h4>
     </div>
+    <div class="pa-5" style="position: absolute; bottom: 0; left: 0">
+      <p>
+        <router-link v-for="year in values.year" :key="year" :title="year"
+          class="d-inline-block pr-4 text-h4 white--text text-decoration-none"
+          :to="{name: 'list', query: {year: year}}">{{year}}</router-link>
+      </p>
+      <p>
+        <router-link v-for="nick in nickNames" :key="nick" :title="nick"
+          class="d-inline-block pr-3 text-h5 white--text text-decoration-none"
+          :to="{name: 'list', query: {nick: nick}}">{{nick}}</router-link>
+      </p>
+      <p class="hidden-sm-and-down">
+        <router-link v-for="tag in values.tags" :key="tag" :title="tag"
+          class="d-inline-block pr-2 text-body-1 white--text text-decoration-none"
+          :to="{name: 'list', query: {tags: tag}}">{{tag}}</router-link>
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 import common from '@/helpers/mixins'
 
 export default {
   name: 'Home',
   mixins: [common],
   computed: {
-    ...mapState('app', ['last', 'bucket']),
+    ...mapState('app', ['last', 'values', 'bucket']),
+    ...mapGetters('app', ['nickNames']),
     count () { return this.bucket.count },
     query () {
       return {
