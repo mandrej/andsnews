ef99ab4fd6b21093836a8b549b8581a46f4952b2
Milan Andrejevic <milan.andrejevic@gmail.com>
2022-03-13 13:49:44 +0100

    manifest, icons cont.

--- vue3/vite.config.js
+++ vue3/vite.config.js
@@ -26,6 +26,63 @@ export default defineConfig({
     VitePWA({
       strategies: "injectManifest",
       registerType: "autoUpdate",
+      manifest: {
+        $schema: "https://json.schemastore.org/web-manifest-combined.json",
+        start_url: "https://ands.appspot.com/",
+        name: "And\u0440\u0435\u0458\u0435\u0432\u0438\u045b\u0438",
+        short_name: "ANDS",
+        display: "standalone",
+        background_color: "#222",
+        theme_color: "#fff",
+        icons: [
+          {
+            src: "/icons/ands-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-60x60.png",
+            sizes: "60x60",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-76x76.png",
+            sizes: "76x76",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-120x120.png",
+            sizes: "120x120",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+          },
+          {
+            src: "/icons/ands-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/icons/aperture.svg",
+            type: "image/svg+xml",
+            purpose: "any maskable",
+          },
+        ],
+        gcm_sender_id: "103953800507",
+      },
     }),
   ],
   envPrefix: "VUE_",
