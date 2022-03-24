d05ef24f49abdd6537b6cb4e3f005fd2c52329a9
Milan Andrejevic <milan.andrejevic@gmail.com>
2022-03-24 12:06:38 +0100

    USE DIALOG PLUGIN

--- vue3/src/components/Edit.vue
+++ vue3/src/components/Edit.vue
@@ -14,97 +14,105 @@
           <q-btn class="gt-sm" v-if="user.isAdmin" flat label="Read Exif" @click="getExif" />
         </div>
         <div>{{ humanStorageSize(tmp.size) }} {{ linearDim(tmp) }}</div>
-        <q-btn color="primary" label="Submit" @click="onOKClick" />
+        <q-btn color="primary" type="submit" label="Submit" @click="onOKClick" />
       </q-toolbar>
       <q-card-section>
-        <div class="row q-col-gutter-md">
-          <div class="col-xs-12 col-sm-4 gt-xs">
-            <q-img :src="smallsized + tmp.filename" :ratio="1" />
-          </div>
-          <div class="col-xs-12 col-sm-8 col-8">
-            <q-input
-              v-model="tmp.headline"
-              label="Headline"
-              :rules="[
-                val => !!val || '* Required',
-              ]"
-              lazy-rules
-            />
-            <q-input v-model="tmp.filename" label="Filename" readonly />
-            <q-select v-model="tmp.email" :options="values.email" label="Author"></q-select>
-            <q-input v-model="tmp.date" label="Date taken">
-              <template v-slot:prepend>
-                <q-icon name="event" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="tmp.date" mask="YYYY-MM-DD HH:mm">
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-date>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-              <template v-slot:append>
-                <q-icon name="access_time" class="cursor-pointer">
-                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
-                    <q-time v-model="tmp.date" mask="YYYY-MM-DD HH:mm" format24h>
-                      <div class="row items-center justify-end">
-                        <q-btn v-close-popup label="Close" color="primary" flat />
-                      </div>
-                    </q-time>
-                  </q-popup-proxy>
-                </q-icon>
-              </template>
-            </q-input>
-          </div>
+        <q-form
+          @submit="onOKClick"
+          autocorrect="off"
+          autocapitalize="off"
+          autocomplete="off"
+          spellcheck="false"
+        >
+          <div class="row q-col-gutter-md">
+            <div class="col-xs-12 col-sm-4 gt-xs">
+              <q-img :src="smallsized + tmp.filename" :ratio="1" />
+            </div>
+            <div class="col-xs-12 col-sm-8 col-8">
+              <q-input
+                v-model="tmp.headline"
+                label="Headline"
+                :rules="[
+                  val => !!val || '* Required',
+                ]"
+                lazy-rules
+              />
+              <q-input v-model="tmp.filename" label="Filename" readonly />
+              <q-select v-model="tmp.email" :options="values.email" label="Author"></q-select>
+              <q-input v-model="tmp.date" label="Date taken">
+                <template v-slot:prepend>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                      <q-date v-model="tmp.date" mask="YYYY-MM-DD HH:mm">
+                        <div class="row items-center justify-end">
+                          <q-btn v-close-popup label="Close" color="primary" flat />
+                        </div>
+                      </q-date>
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+                <template v-slot:append>
+                  <q-icon name="access_time" class="cursor-pointer">
+                    <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                      <q-time v-model="tmp.date" mask="YYYY-MM-DD HH:mm" format24h>
+                        <div class="row items-center justify-end">
+                          <q-btn v-close-popup label="Close" color="primary" flat />
+                        </div>
+                      </q-time>
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
+            </div>
 
-          <div class="col-12">
-            <Complete
-              :modelValue="tmp.tags"
-              :options="values.tags"
-              canadd
-              multiple
-              label="Tags"
-              @update:model-value="newValue => tmp.tags = newValue"
-            />
-          </div>
-          <div class="col-xs-12 col-sm-6">
-            <Complete
-              :modelValue="tmp.model"
-              :options="values.model"
-              label="Camera Model"
-              @update:model-value="newValue => tmp.model = newValue"
-            />
-          </div>
-          <div class="col-xs-12 col-sm-6">
-            <Complete
-              :modelValue="tmp.lens"
-              :options="values.lens"
-              label="Camera Lens"
-              @update:model-value="newValue => tmp.lens = newValue"
-            />
-          </div>
-          <div class="col-xs-6 col-sm-4">
-            <q-input v-model="tmp.focal_length" type="number" label="Focal length" />
-          </div>
+            <div class="col-12">
+              <Complete
+                :modelValue="tmp.tags"
+                :options="values.tags"
+                canadd
+                multiple
+                label="Tags"
+                @update:model-value="newValue => tmp.tags = newValue"
+              />
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <Complete
+                :modelValue="tmp.model"
+                :options="values.model"
+                label="Camera Model"
+                @update:model-value="newValue => tmp.model = newValue"
+              />
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <Complete
+                :modelValue="tmp.lens"
+                :options="values.lens"
+                label="Camera Lens"
+                @update:model-value="newValue => tmp.lens = newValue"
+              />
+            </div>
+            <div class="col-xs-6 col-sm-4">
+              <q-input v-model="tmp.focal_length" type="number" label="Focal length" />
+            </div>
 
-          <div class="col-xs-6 col-sm-4">
-            <q-input v-model="tmp.iso" type="number" label="ISO [ASA]" />
-          </div>
-          <div class="col-xs-6 col-sm-4">
-            <q-input v-model="tmp.aperture" type="number" step="0.1" label="Aperture" />
-          </div>
-          <div class="col-xs-6 col-sm-4">
-            <q-input v-model="tmp.shutter" label="Shutter [s]" />
-          </div>
+            <div class="col-xs-6 col-sm-4">
+              <q-input v-model="tmp.iso" type="number" label="ISO [ASA]" />
+            </div>
+            <div class="col-xs-6 col-sm-4">
+              <q-input v-model="tmp.aperture" type="number" step="0.1" label="Aperture" />
+            </div>
+            <div class="col-xs-6 col-sm-4">
+              <q-input v-model="tmp.shutter" label="Shutter [s]" />
+            </div>
 
-          <div class="col-xs-6 col-sm-4">
-            <q-input v-model="tmp.loc" label="Location [latitude, longitude]" />
+            <div class="col-xs-6 col-sm-4">
+              <q-input v-model="tmp.loc" label="Location [latitude, longitude]" />
+            </div>
+            <div class="col-xs-6 col-sm-4 col-4 q-mt-sm">
+              <q-checkbox v-model="tmp.flash" label="Flash fired?" />
+            </div>
           </div>
-          <div class="col-xs-6 col-sm-4 col-4 q-mt-sm">
-            <q-checkbox v-model="tmp.flash" label="Flash fired?" />
-          </div>
-        </div>
+        </q-form>
       </q-card-section>
     </q-card>
   </q-dialog>
