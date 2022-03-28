fef5cab92de341caef8450d6bc418c2acd94fd0d
Milan Andrejevic <milan.andrejevic@gmail.com>
2022-03-28 11:17:42 +0200

    QUASAR deploy

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -22,16 +22,6 @@ const updateSW = registerSW({
 app.use(Quasar, {
   plugins: { Notify, Dialog }, // import Quasar plugins and add here
   iconSet: quasarIconSet,
-  config: {
-    brand: {
-      // primary: '#e46262',
-      // ... or all other brand colors
-    },
-    // notify: {}, default set of options for Notify Quasar plugin
-    // loading: {...}, // default set of options for Loading Quasar plugin
-    // loadingBar: { ... }, // settings for LoadingBar Quasar plugin
-    // ..and many more (check Installation card on each Quasar component/directive/plugin)
-  },
 });
 app.use(VueGtag, {
   property: {
