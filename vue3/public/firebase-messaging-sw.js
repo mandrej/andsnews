a68bcaa14c68bf769eb1b388903c0e04a0661d8f
Milan Andrejevic <milan.andrejevic@gmail.com>
2022-07-20 16:48:41 +0200

    firebase: ^9.9.0

--- vue3/public/firebase-messaging-sw.js
+++ vue3/public/firebase-messaging-sw.js
@@ -1,14 +1,10 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.10/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.6.10/firebase-messaging-compat.js"
-);
+import { initializeApp } from "firebase/app";
+import { getMessaging } from "firebase/messaging/sw";
 
-firebase.initializeApp({
+const firebaseApp = initializeApp({
   apiKey: "AIzaSyBj5uKo0P_mir_ChQ_syx_kUQ_g7nkNy6M",
   authDomain: "andsnews.firebaseapp.com",
   databaseURL: "https://andsnews.firebaseio.com",
@@ -17,4 +13,4 @@ firebase.initializeApp({
   messagingSenderId: "719127177629",
   appId: "1:719127177629:web:5e1335058fa70551186774",
 });
-const messaging = firebase.messaging();
+const messaging = getMessaging(firebaseApp);
