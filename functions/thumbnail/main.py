f6cb3766e27c74e9bab81bd34bb1543ae7679aea
mandrejevic <milan.andrejevic@gmail.com>
2023-02-13 11:29:51 +0100

    thumbnail set-env-vars

--- functions/thumbnail/main.py
+++ functions/thumbnail/main.py
@@ -1,22 +1,38 @@
+import os
 import functions_framework
 from io import BytesIO
 from google.cloud import storage
+from google.cloud.exceptions import NotFound
 from PIL import Image
 
 storage_client = storage.Client()
-thumb_bucket = storage_client.get_bucket('thumbnails400')
+size = os.environ.get("MAX_SIZE", None)
+thumbnails = os.environ.get("THUMBNAILS", None)
 
 
 @functions_framework.cloud_event
 def make2(cloud_event):
+    if thumbnails is None:
+        print(f"No THUMBNAILS given")
+        return
+    else:
+        try:
+            dest_bucket = storage_client.get_bucket(thumbnails)
+        except NotFound:
+            dest_bucket = storage_client.create_bucket(thumbnails, location="us")
+
+    if size is None:
+        print(f"No MAX_SIZE given")
+        return
+    else:
+        max_size = int(size)
+
     data = cloud_event.data
     name = data["name"]
-    print(f"File finalized: {name}")
 
-    size = 400
-    # skip if already exists
-    thumb = thumb_bucket.get_blob(data['name'])
+    thumb = dest_bucket.get_blob(data['name'])
     if thumb:
+        print(f"Already exist: {name}")
         return
 
     out = BytesIO()
@@ -25,7 +41,7 @@ def make2(cloud_event):
     if blob:
         bytes = blob.download_as_bytes()
         im = Image.open(BytesIO(bytes))
-        im.thumbnail((size, size), Image.BICUBIC)
+        im.thumbnail((max_size, max_size), Image.BICUBIC)
         icc_profile = im.info.get('icc_profile')
         if icc_profile:
             im.save(out, im.format, icc_profile=icc_profile)
@@ -33,22 +49,31 @@ def make2(cloud_event):
             im.save(out, im.format)
 
         # Upload to destination storage
-        thumb = thumb_bucket.blob(data['name'])
+        thumb = dest_bucket.blob(data['name'])
         thumb.upload_from_string(
             out.getvalue(), content_type=data['contentType'])
         thumb.cache_control = blob.cache_control
         thumb.patch()
+        print(f"File finalized: {name}")
 
 
 @functions_framework.cloud_event
 def remove2(cloud_event):
+    if thumbnails is None:
+        print(f"No THUMBNAILS given")
+        return
+    else:
+        dest_bucket = storage_client.get_bucket(thumbnails)
+
     data = cloud_event.data
     name = data["name"]
-    print(f"File deleted: {name}")
 
-    thumb = thumb_bucket.get_blob(data['name'])
+    thumb = dest_bucket.get_blob(data['name'])
     if thumb:
         thumb.delete()
+        print(f"File deleted: {name}")
+    else:
+        print(f"File does not exist: {name}")
 
 
 """
@@ -60,6 +85,8 @@ gcloud functions deploy make2 \
 --entry-point="make2" \
 --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
 --trigger-event-filters="bucket=andsnews.appspot.com" \
+--set-env-vars="MAX_SIZE=400" \
+--set-env-vars="THUMBNAILS=thumbnails400" \
 --trigger-location="us" \
 --region="us-central1"
 
@@ -69,6 +96,7 @@ gcloud functions deploy remove2 \
 --entry-point="remove2" \
 --trigger-event-filters="type=google.cloud.storage.object.v1.deleted" \
 --trigger-event-filters="bucket=andsnews.appspot.com" \
+--set-env-vars="THUMBNAILS=thumbnails400" \
 --trigger-location="us" \
 --region="us-central1"
 """
