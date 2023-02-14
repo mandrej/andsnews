f86c61bbd6f195e0309114c9b0b15691b74b76a8
mandrejevic <milan.andrejevic@gmail.com>
2023-02-14 18:29:03 +0100

    cont.

--- functions/gothumb/main.go
+++ functions/gothumb/main.go
@@ -6,33 +6,45 @@ import (
 	"image"
 	"image/jpeg"
 	"log"
-	"time"
+	"os"
+	"strconv"
 
 	"cloud.google.com/go/storage"
 	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/nfnt/resize"
 )
 
-const size = 400
 const quality = 85
-const destination = "thumbnails400"
+var client *storage.Client
+var maxSize uint64
+var destination string
+
+func init() {
+  var err error
+  var ok bool
+
+  if client, err = storage.NewClient(context.Background()); err != nil {
+    log.Fatalf("storage.NewClient: %v", err)
+    os.Exit(1)
+  }
+  if size, ok := os.LookupEnv("MAX_SIZE"); !ok {
+    log.Fatalf("No THUMBNAILS given")
+  } else {
+    maxSize, _ = strconv.ParseUint(size, 10, 64)
+  }
+  if destination, ok = os.LookupEnv("THUMBNAILS"); !ok {
+    log.Fatalf("No THUMBNAILS given")
+    os.Exit(1)
+  }
+}
 
 type StorageObjectData struct {
   Bucket         string    `json:"bucket,omitempty"`
   Name           string    `json:"name,omitempty"`
-  Metageneration int64     `json:"metageneration,string,omitempty"`
-  TimeCreated    time.Time `json:"timeCreated,omitempty"`
-  Updated        time.Time `json:"updated,omitempty"`
-}
-
-var client *storage.Client
-
-func init() {
-  var err error
-  client, err = storage.NewClient(context.Background())
-  if err != nil {
-    log.Fatalf("storage.NewClient: %v", err)
-  }
+  // Metageneration int64     `json:"metageneration,string,omitempty"`
+  // TimeCreated    time.Time `json:"timeCreated,omitempty"`
+  // Updated        time.Time `json:"updated,omitempty"`
+  ContentType    string    `json:"content-type,omitempty"`
 }
 
 func eventData(e event.Event) StorageObjectData {
@@ -44,18 +56,16 @@ func eventData(e event.Event) StorageObjectData {
     log.Printf("event.DataAs: %v", err)
   }
   log.Printf("Bucket: %s", data.Bucket)
-  log.Printf("File: %s", data.Name)
-  log.Printf("Metageneration: %d", data.Metageneration)
-  log.Printf("Created: %s", data.TimeCreated)
-  log.Printf("Updated: %s", data.Updated)
+  log.Printf("Name: %s", data.Name)
+  log.Printf("ContentType: %s", data.ContentType)
 
   return data
-
 }
 
 func make(ctx context.Context, e event.Event) error {
   data := eventData(e)
   inputBlob := client.Bucket(data.Bucket).Object(data.Name)
+  outputBlob := client.Bucket(destination).Object(data.Name)
 
   r, err := inputBlob.NewReader(ctx)
 	if err != nil {
@@ -75,15 +85,14 @@ func make(ctx context.Context, e event.Event) error {
 
   var newImage image.Image
 	if im.Width >= im.Height {
-		newImage = resize.Resize(size, 0, img, resize.Lanczos3)
+		newImage = resize.Resize(uint(maxSize), 0, img, resize.Lanczos3)
 	} else {
-		newImage = resize.Resize(0, size, img, resize.Lanczos3)
+		newImage = resize.Resize(0, uint(maxSize), img, resize.Lanczos3)
 	}
 
   var opts jpeg.Options
 	opts.Quality = quality
 
-  outputBlob := client.Bucket(destination).Object(data.Name)
   w := outputBlob.If(storage.Conditions{DoesNotExist: true}).NewWriter(ctx)
   // defer w.Close()
 
@@ -100,7 +109,7 @@ func make(ctx context.Context, e event.Event) error {
 
 func remove(ctx context.Context, e event.Event) error {
   data := eventData(e)
-  blob := client.Bucket(destination).Object(data.Name)
+  blob := client.Bucket(destination).Object(data.Name).If(storage.Conditions{DoesNotExist: false})
   if err := blob.Delete(ctx); err != nil {
     return fmt.Errorf("Bucket delete: %v", err)
   }
@@ -108,21 +117,26 @@ func remove(ctx context.Context, e event.Event) error {
 }
 
 /*
-gcloud functions deploy gothumb \
+cd ~/work/andsnews/functions/thumbnail
+
+gcloud functions deploy make2 \
 --gen2 \
 --runtime=go119 \
---region=us-central1 \
---source=. \
---entry-point=make \
+--entry-point="make2" \
 --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
---trigger-event-filters="bucket=andsnews.appspot.com"
+--trigger-event-filters="bucket=andsnews.appspot.com" \
+--set-env-vars="MAX_SIZE=400" \
+--set-env-vars="THUMBNAILS=thumbnails400" \
+--trigger-location="us" \
+--region="us-central1"
 
-gcloud functions deploy gothumb \
+gcloud functions deploy remove2 \
 --gen2 \
---runtime=us-central1 \
---region=REGION \
---source=. \
---entry-point=remove \
+--runtime=go119 \
+--entry-point="remove2" \
 --trigger-event-filters="type=google.cloud.storage.object.v1.deleted" \
---trigger-event-filters="bucket=andsnews.appspot.com"
+--trigger-event-filters="bucket=andsnews.appspot.com" \
+--set-env-vars="THUMBNAILS=thumbnails400" \
+--trigger-location="us" \
+--region="us-central1"
 */
