c26b4149d1a324a3d7104226cecb139bfab15706
mandrejevic <milan.andrejevic@gmail.com>
2023-05-20 16:58:08 +0200

    .

--- vue3/proxy.js
+++ vue3/proxy.js
@@ -3,6 +3,7 @@ module.exports = [
     path: "/api",
     rule: {
       target: "http://127.0.0.1:6060/api",
+      changeOrigin: false,
       pathRewrite: { "^/api": "" },
     },
   },
--- vue3/quasar.config.js
+++ vue3/quasar.config.js
@@ -83,7 +83,6 @@ module.exports = configure(function (/* ctx */) {
       proxy: {
         "/api": {
           target: "http://127.0.0.1:6060/api",
-          changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
